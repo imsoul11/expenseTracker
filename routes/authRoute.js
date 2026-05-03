ba6882eda0c01109b5378de8c604f6d02a6af0bd
Antas Jain <antasjain2004@gmail.com>
2026-05-04 01:00:24 +0530

    Improving auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const asyncHandler = require("../middleware/asyncHandler");
+const { validateRegister, validateLogin } = require("../middleware/validateRequest");
 
-const { registerUser, loginUser, logoutUser, refresh } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refresh,
+  getCurrentUser,
+} = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/logout", logoutUser);
-router.post("/refresh", refresh);
+router.post("/register", validateRegister, asyncHandler(registerUser));
+router.post("/login", validateLogin, asyncHandler(loginUser));
+router.post("/logout", asyncHandler(logoutUser));
+router.post("/refresh", asyncHandler(refresh));
+router.get("/me", authMiddleware, asyncHandler(getCurrentUser));
 
 module.exports = router;
